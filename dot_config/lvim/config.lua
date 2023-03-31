00ee68a2bca3804bae7514c98a3085f7cb54e2e8
blennster <emil.blennow99@gmail.com>
2023-03-31 11:56:23 +0200

    update lvim config

--- dot_config/lvim/config.lua
+++ dot_config/lvim/config.lua
@@ -80,7 +80,7 @@ lvim.builtin.treesitter.auto_install = true
 local formatters = require("lvim.lsp.null-ls.formatters")
 formatters.setup({
   -- { command = "stylua" },
-  { command = "autopep8" },
+  { command = "autopep8", filetypes = { "python" } },
   { command = "prettier" },
   -- {
   --   command = "prettier",
@@ -106,11 +106,35 @@ formatters.setup({
 -- }
 lvim.plugins = {
   "tpope/vim-surround",
+  "mrjones2014/nvim-ts-rainbow",
+  {
+    "rmagatti/goto-preview",
+    config = function()
+      require('goto-preview').setup {
+        width = 120,             -- Width of the floating window
+        height = 25,             -- Height of the floating window
+        default_mappings = true, -- Bind default mappings
+        debug = false,           -- Print debug information
+        opacity = nil,           -- 0-100 opacity level of the floating window where 100 is fully transparent.
+        post_open_hook = nil     -- A function taking two arguments, a buffer and a window to be ran as a hook.
+        -- You can use "default_mappings = true" setup option
+        -- Or explicitly set keybindings
+        -- vim.cmd("nnoremap gpd <cmd>lua require('goto-preview').goto_preview_definition()<CR>")
+        -- vim.cmd("nnoremap gpi <cmd>lua require('goto-preview').goto_preview_implementation()<CR>")
+        -- vim.cmd("nnoremap gP <cmd>lua require('goto-preview').close_all_win()<CR>")
+      }
+    end
+  },
+  {
+    "metakirby5/codi.vim",
+    cmd = "Codi",
+  },
   -- make sure to change the value of `timeoutlen` if it's not triggering correctly, see https://github.com/tpope/vim-surround/issues/117
   -- setup = function()
   --    vim.o.timeoutlen = 500
   -- end
 }
+lvim.builtin.treesitter.rainbow.enable = true
 
 -- This was annoying
 -- vim.opt.foldmethod = "expr"
