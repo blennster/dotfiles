a7056a7ef1be2d922e1457b71b3cd34eddafcfb1
Emil Blennow <emil.blennow@sony.com>
2023-07-19 09:44:14 +0200

    update work profile

--- home-work.nix
+++ home-work.nix
@@ -1,5 +1,8 @@
-{...}: {
+{lib, ...}: {
   home.username = "s7000033439";
   home.homeDirectory = "/home/SONY/s7000033439";
+  programs.kitty.settings.shell = "/usr/bin/zsh";
+  programs.kitty.font.size = lib.mkForce 12;
+  programs.git.signing.signByDefault = lib.mkForce false;
   imports = [./common.nix];
 }
