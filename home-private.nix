bb28a93c1d08e1c68007a9c69246e2e9b394ca47
Emil Blennow <emil.blennow99@gmail.com>
2023-07-22 17:32:23 +0200

    add gnome config

--- home-private.nix
+++ home-private.nix
@@ -1,4 +1,4 @@
-{...}: {
+{lib, ...}: {
   home.username = "emil";
   home.homeDirectory = "/home/emil";
   programs.git = {
@@ -18,11 +18,37 @@
   home.file = {
     ".gnupg/gpg-agent.conf".text = "pinentry-program /run/current-system/sw/bin/pinentry";
   };
+
   dconf.settings = with lib.hm.gvariant; {
+    "org/gnome/extensions" = {
+      enabled-extensions = ["appindicatorsupport@rgcjonas.gmail.com" "gsconnect@andyholmes.github.io" "syncthing@gnome.2nv2u.com"];
+    };
+    "org/gtk/settings/file-chooser" = {sort-directories-first = true;};
+    "org/gtk/gtk4/settings/file-chooser" = {sort-directories-first = true;};
+    "org/gnome/desktop/wm/keybindings" = {
+      switch-applications = ["<Super>Tab"];
+      switch-applications-backward = ["<Shift><Super>Tab"];
+      switch-windows = ["<Alt>Tab"];
+      switch-windows-backward = ["<Shift><Alt>Tab"];
+    };
     "org/gnome/desktop/background" = {
       picture-uri = "/home/emil/.local/share/wallpapers/nix-wallpaper-nineish.svg";
       picture-uri-dark = "/home/emil/.local/share/wallpapers/nix-wallpaper-nineish-dark-gray.svg";
     };
+    "org/gnome/desktop/input-sources" = {
+      sources = [(mkTuple ["xkb" "se"])];
+    };
+    "org/gnome/desktop/interface" = {
+      monospace-font-name = "IosevkaTerm NFM 12";
+    };
+    "org/gnome/desktop/peripherals/touchpad" = {
+      tap-to-click = true;
+      two-finger-scrolling-enabled = true;
+    };
+    "org/gnome/desktop/peripherals/mouse" = {
+      accel-profile = "flat";
+    };
+    "org/gnome/TextEditor" = {restore-session = false;};
   };
   imports = [./common.nix];
 }
