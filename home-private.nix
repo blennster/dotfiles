b9f44e89a0ceb76df74da9b4210bdf409c8d7681
Emil Blennow <emil.blennow99@gmail.com>
2023-07-22 20:29:34 +0200

    update dconf again

--- home-private.nix
+++ home-private.nix
@@ -48,6 +48,9 @@
       accel-profile = "flat";
     };
     "org/gnome/TextEditor" = {restore-session = false;};
+    "org/gnome/settings-daemon/plugins/media-keys" = {
+      custom-keybindings = ["/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/" "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/"];
+    };
     "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
       binding = "<Control><Alt>t";
       command = "kitty";
