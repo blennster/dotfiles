4ce4226e57bc1b3e482a02a5b60bdcc0bf898add
Emil Blennow <emil.blennow@sony.com>
2023-07-25 10:33:45 +0200

    remove firefox from common

--- common.nix
+++ common.nix
@@ -31,8 +31,6 @@
     ++ (import ./pkgs.nix pkgs).prog
     ++ (import ./pkgs.nix pkgs).nvim;
 
-  programs.firefox.enable = true;
-
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
   home.file = {
