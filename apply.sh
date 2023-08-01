4d260af9a7e0b29b1dd7fdbe84c4a49f5881999c
Emil Blennow <emil.blennow99@gmail.com>
2023-08-01 15:19:39 +0200

    update apply script

--- apply.sh
+++ apply.sh
@@ -9,5 +9,6 @@ sudo cp "hardware/$(hostname).nix" /etc/nixos/hardware-configuration.nix
 if [[ -z $1 ]]; then
 	nixos-rebuild --help
 else
-	sudo nixos-rebuild "$1"
+	# shellcheck disable=2048,2086
+	sudo nixos-rebuild $*
 fi
