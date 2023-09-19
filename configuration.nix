85701af71fe7fd305e8d7fff3a6e4b7a99ddc0e1
Emil Blennow <emil.blennow99@gmail.com>
2023-09-19 14:36:45 +0200

    support nvidia desktop

--- configuration.nix
+++ configuration.nix
@@ -167,18 +167,6 @@ in {
     };
   };
 
-  dbus-sway-environment = pkgs.writeTextFile {
-    name = "dbus-sway-environment";
-    destination = "/bin/dbus-sway-environment";
-    executable = true;
-
-    text = ''
-      dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=sway
-      systemctl --user stop pipewire pipewire-media-session xdg-desktop-portal xdg-desktop-portal-wlr
-      systemctl --user start pipewire pipewire-media-session xdg-desktop-portal xdg-desktop-portal-wlr
-    '';
-  };
-
   programs.sway = {
     enable = true;
     wrapperFeatures.gtk = true;
--- hardware/ms7b86.nix
+++ hardware/ms7b86.nix
@@ -50,6 +50,32 @@
     driSupport32Bit = true;
   };
 
+  programs.sway = {
+    extraSessionCommands = ''
+      # Hardware cursors not yet working on wlroots
+      export WLR_NO_HARDWARE_CURSORS=1
+      # Set wlroots renderer to Vulkan to avoid flickering
+      # export WLR_RENDERER=vulkan
+      # General wayland environment variables
+      export XDG_SESSION_TYPE=wayland
+      export QT_QPA_PLATFORM=wayland
+      export QT_WAYLAND_DISABLE_WINDOWDECORATION=1
+      # Firefox wayland environment variable
+      export MOZ_ENABLE_WAYLAND=1
+      export MOZ_USE_XINPUT2=1
+      # OpenGL Variables
+      export GBM_BACKEND=nvidia-drm
+      export __GL_GSYNC_ALLOWED=0
+      export __GL_VRR_ALLOWED=0
+      export __GLX_VENDOR_LIBRARY_NAME=nvidia
+      # Xwayland compatibility
+      export XWAYLAND_NO_GLAMOR=1
+    '';
+    extraOptions = [
+      "--unsupported-gpu"
+    ];
+  };
+
   # NVIDIA drivers are unfree.
   nixpkgs.config.allowUnfreePredicate = pkg:
     builtins.elem (lib.getName pkg) [
@@ -69,6 +95,8 @@
 
     # Enable the nvidia settings menu
     nvidiaSettings = false; # Using wayland
+
+    open = true;
 
     # Optionally, you may need to select the appropriate driver version for your specific GPU.
     # package = config.boot.kernelPackages.nvidiaPackages.stable;
