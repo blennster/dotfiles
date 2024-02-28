4135c08245faf9ab573fcb516ef0ca84c6cc019c
Emil Blennow <emil.blennow@sony.com>
2024-02-28 10:46:07 +0100

    missing link for tmux

--- stow.sh
+++ stow.sh
@@ -7,6 +7,7 @@ modules=(shell others scripts sway)
 case $USER in
     "s7000033439")
         stow -v work
+        ln -f -s ~/.config/tmux/tmux.conf ~/.tmux.conf
         ;;
     *)
         stow -v home
