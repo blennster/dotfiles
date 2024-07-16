02be3a1e74ba5817302dacec6fa247a26f9f4ccd
Emil Blennow <emil.blennow@svep.se>
2024-07-16 11:11:43 +0200

    formatting

--- stow.sh
+++ stow.sh
@@ -5,15 +5,14 @@ set -eu
 modules=(shell others scripts sway)
 
 case $USER in
-    "now")
-        modules=(work shell)
-        ;;
-    *)
-        modules=(home "${modules[@]}")
-        ;;
+"now")
+    modules=(work shell)
+    ;;
+*)
+    modules=(home "${modules[@]}")
+    ;;
 esac
 
 for module in "${modules[@]}"; do
     stow --dotfiles -v "$module"
 done
-
